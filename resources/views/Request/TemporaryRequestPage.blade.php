de4b13768a65139efb60cad2b85580388593d450
eyuel <eyouwor@gmail.com>
2024-09-05 19:28:17 +0530

    temporary request form update

--- resources/views/Request/TemporaryRequestPage.blade.php
+++ resources/views/Request/TemporaryRequestPage.blade.php
@@ -50,10 +50,16 @@
                                     </li>
                                     <li class="nav-item">
                                         <a href="#finish-3" data-bs-toggle="tab" data-toggle="tab" class="nav-link rounded-0 py-2">
-                                            <i class=" ri-suitcase-3-fill fw-normal fs-20 align-middle me-1"></i>
-                                            <span class="d-none d-sm-inline">Cargo</span>
+                                            <i class=" ri-information-fill fw-normal fs-20 align-middle me-1"></i>
+                                            <span class="d-none d-sm-inline">Info</span>
                                         </a>
                                     </li>
+                                    <!-- <li class="nav-item">
+                                        <a href="#finish-4" data-bs-toggle="tab" data-toggle="tab" class="nav-link rounded-0 py-2">
+                                            <i class=" ri-suitcase-3-fill fw-normal fs-20 align-middle me-1"></i>
+                                            <span class="d-none d-sm-inline">Extras</span>
+                                        </a>
+                                    </li> -->
                                 </ul>
 
                                 <div class="tab-content b-0 mb-0">
@@ -126,6 +132,14 @@
                                                     data-date-container="#datepicker1">
                                             </div>
                                         </div>
+                                        <div class="position-relative mb-3">
+                                            <div class="mb-6 position-relative" id="datepicker1">
+                                                <label class="form-label">How many Days</label>
+                                                <input type="number" class="form-control" name="how_many_days" placeholder="Enter the Duration"
+                                                    data-provide="datepicker" data-date-today-highlight="true"
+                                                    data-date-container="#datepicker1">
+                                            </div>
+                                        </div>
                                     </div> 
                                         <ul class="pager wizard mb-0 list-inline">
                                             <li class="previous list-inline-item">
@@ -165,55 +179,124 @@
                                                     <a href="#finish-3" type="button" class="btn btn-info">Add More Info <i class="ri-arrow-right-line ms-1"></i></a>
                                                 </li>
                                             </ul>
-                                </div>   
+                                </div>  
                                 <div class="tab-pane" id="finish-3">
                                     <div class="row">
-
-                                        <p class="mb-1 fw-bold text-muted">Select People</p>
-                                        <select id="multiSelect" name="people_id[]" class="select2 form-control select2-multiple" data-toggle="select2"
-                                            multiple="multiple" data-placeholder="Select People ...">
-                                            <optgroup label="Users/Employees">
-                                                @foreach($users as $user);
-                                                <option value="{{$user->id}}"><p style="color:black">{{$user->first_name}}</option>
-                                                @endforeach
-                                            </optgroup>
-                                        </select>
-                                        <div id="selectedValues" class="mt-2"></div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <div class="mb-3 position-relative" id="datepicker1">
-                                                <label for="itemName" class="form-label">Item Name:</label>
-                                                <input type="text" class="form-control" placeholder="Add Item"
-                                                id="itemName">
+                                        <h6 class="fs-15 mt-3">Do you need driver</h6>
+                                        <div class="mt-2">
+                                            <div class="form-check form-check-inline">
+                                                <input type="checkbox" class="form-check-input"  name="with_driver" value="1">
+                                                <label class="form-check-label" >Yes</label>
+                                            </div>
+                                            <div class="form-check form-check-inline">
+                                                <input type="checkbox" class="form-check-input" name="with_driver" value="0">
+                                                <label class="form-check-label" >No</label>
                                             </div>
                                         </div>
-
-                                        <div class="col-lg-6">
-                                            <div class="mb-3 position-relative" id="datepicker2">
-                                                <label for="itemWeight" class="form-label">Weight (kg):</label>
-                                                <input type="text" class="form-control" placeholder="Add weight"
-                                                id="itemWeight" step="0.01" min="0" >
+                                        <h6 class="fs-15 mt-3">In/Out Town</h6>
+                                        <div class="mt-2">
+                                            <div class="form-check form-check-inline">
+                                                <input type="checkbox" class="form-check-input"  name="in_out_town" value="1">
+                                                <label class="form-check-label">In town</label>
+                                            </div>
+                                            <div class="form-check form-check-inline">
+                                                <input type="checkbox" class="form-check-input" name="in_out_town" value="0">
+                                                <label class="form-check-label" >Out town</label>
                                             </div>
                                         </div>
-                                        <div class="d-flex flex-wrap gap-2 justify-content-center align-items-center">
-                                            <button type="button" class="btn btn-primary rounded-pill" id="addItem">Add</button>
-                                        </div>
+                                        <h6 class="fs-15 mt-3">Is there a Passenger or Package?</h6>
+                                        <div class="mt-2">
+                                            <div class="form-check form-check-inline">
+                                                <input type="checkbox" class="form-check-input"  name="package" value="1">
+                                                <label class="form-check-label">Yes</label>
+                                            </div>
+                                            <div class="form-check form-check-inline">
+                                                <input type="checkbox" class="form-check-input" name="package" value="0">
+                                                <label class="form-check-label" >No</label>
+                                            </div>
+                                        </div></br>
+                                        <div id="TogglePackage" style="display:none">
+                                            <div class="row">
+
+                                                <p class="mb-1 fw-bold text-muted">Select People</p>
+                                                <select id="multiSelect" name="people_id[]" class="select2 form-control select2-multiple" data-toggle="select2"
+                                                    multiple="multiple" data-placeholder="Select People ...">
+                                                    <optgroup label="Users/Employees">
+                                                        @foreach($users as $user);
+                                                        <option value="{{$user->id}}"><p style="color:black">{{$user->first_name}}</option>
+                                                        @endforeach
+                                                    </optgroup>
+                                                </select>
+                                                <div id="selectedValues" class="mt-2"></div>
+                                            </div>
+
+                                            <div class="row">
+                                                <div class="col-lg-6">
+                                                    <div class="mb-3 position-relative" id="datepicker1">
+                                                        <label for="itemName" class="form-label">Item Name:</label>
+                                                        <input type="text" class="form-control" placeholder="Add Item"
+                                                        id="itemName">
+                                                    </div>
+                                                </div>
+
+                                            <div class="col-lg-6">
+                                                <div class="mb-3 position-relative" id="datepicker2">
+                                                    <label for="itemWeight" class="form-label">Weight (kg):</label>
+                                                    <input type="text" class="form-control" placeholder="Add weight"
+                                                    id="itemWeight" step="0.01" min="0" >
+                                                </div>
+                                            </div>
+                                            <div class="d-flex flex-wrap gap-2 justify-content-center align-items-center">
+                                                <button type="button" class="btn btn-primary rounded-pill" id="addItem">Add</button>
+                                            </div>
                                         </div>
                 
                                         <div id="itemList"></div>
-                                            <ul class="pager wizard mb-0 list-inline mt-1">
-                                                <li class="previous list-inline-item">
-                                                    <button type="button" class="btn btn-light"><i class="ri-arrow-left-line me-1"></i> Back to Profile</button>
-                                                </li>
-                                                <li class="next list-inline-item float-end">
-                                                    <button type="submit" class="btn btn-info">Submit</button>
-                                                </li>
-                                            </ul>  
                                         </div>
-                                </div>  
-                            </div>
+                                    </div> 
+                                    <ul class="pager wizard mb-0 list-inline mt-1">
+                                            <li class="previous list-inline-item">
+                                                <button type="button" class="btn btn-light"><i class="ri-arrow-left-line me-1"></i> Back to Profile</button>
+                                            </li>
+                                            <li class="next list-inline-item float-end">
+                                                <button type="submit" class="btn btn-info">Submit</button>
+                                            </li>
+                                        </ul>  
+                                </div>
+                                  
+                                        <script>
+                                            //  window.onload = function() {
+                                            //         location.reload();
+                                            //     };
+                                            const checkboxes = document.querySelectorAll('input[name="with_driver"], input[name="in_out_town"], input[name="package"]');
+                                            const packageCheckbox = document.querySelector('input[name="package"]');
+                                            const divToToggle = document.getElementById('TogglePackage');
+
+                                                checkboxes.forEach((checkbox) => {
+                                                    checkbox.addEventListener('change', function() {
+                                                        checkboxes.forEach((cb) => {
+                                                            if (cb !== this && cb.name === this.name) {
+                                                                cb.checked = false;
+                                                                divToToggle.style.display = 'none';
+                                                               
+                                                            }
+                                                        });
+                                                    });
+                                                });
+                                               
+                                                packageCheckbox.addEventListener('change', function() {
+                                                    if (divToToggle) {
+                                                        if (packageCheckbox.value === '1') {
+                                                            divToToggle.style.display = 'block'; // Display the div if the checkbox value is 1
+                                                         } else {
+                                                            divToToggle.style.display = 'none';                                              }
+                                                    } 
+                                                });
+                                                                                            
+                                        </script>
+                                    </div></br> <!-- end card-body-->
+                                </div>   
+                                
 
                         </form> 
 
