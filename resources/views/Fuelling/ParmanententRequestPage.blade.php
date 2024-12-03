b8f5a51ef6644d25f0b03c92e4977bd3c34ebe6c
ey0u <eyouwor@gmail.com>
2024-12-03 22:56:06 +0300

    just do it

--- resources/views/Fuelling/ParmanententRequestPage.blade.php
+++ resources/views/Fuelling/ParmanententRequestPage.blade.php
@@ -359,6 +359,9 @@
 
                         if (response.status === 'success' && Array.isArray(response.data) && response.data.length > 0) {
                             // Populate the table and info section
+                            var total_fuel =  response.total_fuel;
+                            var h1 = $('<h4>').append('Total: <span style="text-decoration: underline;font-size:16px;">' + total_fuel + '</span>');
+                            var input = $('<div id="entriesInputContainer"></div>')
                             var table = $('<table class="table table-striped">').append(`
                                 <thead>
                                     <tr>
@@ -391,8 +394,7 @@
                                                 <div class="row">
                                                     <div class="col-6">
                                                         <div class="form-check">
-                                                            <a class="btn btn-secondary btn-sm rounded-pill" title="edit"><i class="ri-edit-line"></i></a>   
-                                                        </div>
+                                                            <button class="btn btn-primary btn-sm update-btn" data-row="${fueling.primary}" data-cost="${fueling.fuel_cost}" data-date="${fueling.fuiling_date}">Update</button>                                                        </div>
                                                     </div>
                                                 </div>
                                             ` : ''} 
@@ -403,16 +405,116 @@
                             });
 
                             cardsContainer.append(table);
+                            cardsContainer.append(h1);
+                            cardsContainer.append(input);
+                            
                         } else {
-                            cardsContainer.html('<p>No inspection data available.</p>');
+                            cardsContainer.html('<p>No  data available.</p>');
                         }
                     },
                     error: function() {
                         var cardsContainer = $('#inspectionCardsContainer');
-                        cardsContainer.html('<p>No inspection data available at the moment. Please check the Plate number!</p>');
+                        cardsContainer.html('<p>No data available at the moment!</p>');
                     }
                 });
             });
+
+            $(document).on('click', '.update-btn', function () {
+                var rowId = $(this).data('row');
+                var rowCost = $(this).data('cost');
+                var rowDate = $(this).data('date');
+        
+                const entriesInputContainer = document.getElementById('entriesInputContainer');
+
+                if (document.getElementById(`entry-${rowId}`)) {
+                    console.log(`Input fields for already exist.`);
+                    return; 
+                }
+
+                const entryDiv = document.createElement('div');
+
+                entryDiv.classList.add('entry', 'mt-3');
+                entryDiv.id = `entry-${rowId}`;
+                                                        
+                entryDiv.innerHTML = `
+                    <div class="row">
+                        
+                        <div class="col-md-4">
+                            <label>Price</label>
+                            <input name="fuel_cost" class="form-control" placeholder="In Birr" type="number" value="${rowCost}" required>
+                        </div>
+                        <div class="col-md-4">
+                            <label>Date</label>
+                            <input name="fuiling_date" class="form-control" placeholder="When" type="date" value="${rowDate}" required>
+                        </div>
+                        <div class="col-md-4">
+                            <label>Attachment</label>
+                            <input name="reciet_attachment" class="form-control" type="file" required>
+                        </div>
+                        <div class="col-md-2">
+                             <button type="button" class="btn btn-primary btn-sm submitInput-btn" data-row="${rowId}"><i class="ri-checkbox-circle-line"></i></button>
+                            <button type="button" class="btn btn-danger btn-sm removeInputEntry">x</button>
+                        </div>
+                    </div>
+                `;
+                
+                
+                entriesInputContainer.appendChild(entryDiv);
+
+            });
+
+            $(document).on('click', '.removeInputEntry', function () {
+                $(this).closest('.entry').remove(); 
+            });
+
+            $(document).on('click', '.submitInput-btn', function () {
+                    var rowId = $(this).data('row'); // Assuming `data-row` attribute is set on the button
+                    const parentRow = $(this).closest('.row'); // Get the parent row container
+
+                    // Retrieve input values
+                    const fuelCost = parentRow.find('input[name="fuel_cost"]').val();
+                    const fuelingDate = parentRow.find('input[name="fuiling_date"]').val();
+                    const receiptInput = parentRow.find('input[name="reciet_attachment"]'); 
+                    const csrfToken = "{{ csrf_token() }}";
+
+                    const receiptAttachment = receiptInput[0].files[0];
+                    
+                    if (!fuelCost || !fuelingDate || !receiptAttachment) {
+                        alert('Please fill all the fields and attach a receipt.');
+                        return;
+                    }
+
+                    const formData = new FormData();
+                    formData.append('_token', csrfToken); 
+                    formData.append('make_primary', rowId); 
+                    formData.append('fuel_cost', fuelCost);
+                    formData.append('fuiling_date', fuelingDate);
+                    formData.append('reciet_attachment', receiptAttachment);
+                  
+
+                    // AJAX request
+                    $.ajax({
+                    url: '/update_entries', 
+                    type: 'POST',
+                    data: formData,
+                    processData: false, 
+                    contentType: false, 
+                    success: function (response) {
+                        if (response.status === 'success') {
+                            alert('Data submitted');
+                            
+                            parentRow.remove(); 
+                        } else {
+                            alert(response.message || 'An error occurred.');
+                        }
+                    },
+                    error: function (xhr, status, error) {
+                        console.error('AJAX Error:', error);
+                        alert('Failed to submit data. Please try again.');
+                    }
+                });
+                });
+
 
             function showFileInIframe(fileUrl) {
                 clearFilePreview(); 
