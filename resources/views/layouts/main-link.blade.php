d933f28d5ad942adfb7527efe07bdf997b96fa3b
ey0u <eyouwor@gmail.com>
2024-12-18 16:13:07 +0300

    just do it

--- resources/views/layouts/main-link.blade.php
+++ resources/views/layouts/main-link.blade.php
@@ -1,12 +1,12 @@
 <head>
     <meta charset="utf-8" />
-    <title>Dashboard</title>
+    <title>Fleet Management</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully responsive admin theme which can be used to build CRM, CMS,ERP etc." name="description" />
     <meta content="Techzaa" name="author" />
 
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/images/aiiLogo.png') }}">
 
     <!-- EthiopianDatepicker css -->
     <link href="{{ asset('assets/css/redmond.calendars.picker.css') }}" rel="stylesheet" />
